ec79375dc3d964d55e3d4e7aa29e78e25f92baf7
MatiasGolubeff7 <matiasgolubeff@gmail.com>
2023-09-19 20:53:17 -0300

    update

--- Client/src/Home/Home.jsx
+++ Client/src/Home/Home.jsx
@@ -1,4 +1,80 @@
-const Home = () => {
-  let hola = "hola";
-  console.log(hola);
+const data = {
+  productos: [
+    {
+      id: 1,
+      nombre: "Camisa",
+      descripcion: "Camisa de algodón de manga larga",
+      precio: 30.0,
+      talla: ["S", "M", "L", "XL"],
+      colores: ["Blanco", "Azul", "Negro"],
+      categoria: "Ropa",
+      imagen: "ruta/imagen_camisa.jpg",
+    },
+    {
+      id: 2,
+      nombre: "Pantalón",
+      descripcion: "Pantalón vaquero de corte recto",
+      precio: 40.0,
+      talla: ["S", "M", "L", "XL"],
+      colores: ["Azul", "Negro", "Gris"],
+      categoria: "Ropa",
+      imagen: "ruta/imagen_pantalon.jpg",
+    },
+    {
+      id: 3,
+      nombre: "Camisa",
+      descripcion: "Camisa de algodón de manga larga",
+      precio: 30.0,
+      talla: ["S", "M", "L", "XL"],
+      colores: ["Blanco", "Azul", "Negro"],
+      categoria: "Ropa",
+      imagen: "ruta/imagen_camisa.jpg",
+    },
+  ],
+  categorias: [
+    {
+      id: 1,
+      nombre: "Ropa",
+    },
+    {
+      id: 2,
+      nombre: "Calzado",
+    },
+    {
+      id: 3,
+      nombre: "Accesorios",
+    },
+  ],
+  usuarios: [
+    {
+      id: 1,
+      nombre: "Usuario 1",
+      email: "usuario1@example.com",
+      direccion: "123 Calle Principal, Ciudad",
+      pedidos: [
+        {
+          id_pedido: 101,
+          productos: [
+            {
+              id_producto: 1,
+              cantidad: 2,
+            },
+            {
+              id_producto: 2,
+              cantidad: 1,
+            },
+          ],
+          total: 100.0,
+          estado: "En proceso",
+        },
+      ],
+    },
+    {
+      id: 2,
+      nombre: "Usuario 2",
+      email: "usuario2@example.com",
+      direccion: "456 Calle Secundaria, Ciudad",
+      pedidos: [],
+    },
+  ],
 };
