a8fdb08f22aa0977cc871ff4e7c9554f571c9a0a
JoseNicolas <villagrajosenicolas@gmail.com>
2023-09-20 15:10:24 -0300

    typesScript

--- Client/src/Home/Home.ts
+++ Client/src/Home/Home.ts
@@ -1,11 +1,48 @@
-const data = {
+interface Productos {
+  id: number;
+  nombre: string;
+  descripcion:string
+  precio: number;
+  talla:[string,string,string,string,string];
+  colores:[string,string,string]
+  categoria: string;
+  imagen: string 
+}
+interface Categoria{
+  id:number;
+  nombre: string;
+}
+interface Usuario_Pedidos_Productos{
+  id_producto:number;
+  cantidad:number
+}
+interface Usuario_Pedidos{
+  id_pedido:number;
+  productos:Usuario_Pedidos_Productos[]
+  total:number;
+  estado:string
+}
+interface Usuario {
+  id:number;
+  nombre:string;
+  email:string;
+  direccion:string;
+  pedidos:Usuario_Pedidos[]
+}
+interface DataRopa {
+  productos: Productos[]
+  categorias: Categoria[]
+  usuarios: Usuario[]
+}
+
+const data:DataRopa = {
   productos: [
     {
       id: 1,
       nombre: "Camisa",
       descripcion: "Camisa de algodón de manga larga",
       precio: 30.0,
-      talla: ["S", "M", "L", "XL"],
+      talla: ["S", "M", "L", "XL","XXL"],
       colores: ["Blanco", "Azul", "Negro"],
       categoria: "Ropa",
       imagen: "ruta/imagen_camisa.jpg",
@@ -15,7 +52,7 @@ const data = {
       nombre: "Pantalón",
       descripcion: "Pantalón vaquero de corte recto",
       precio: 40.0,
-      talla: ["S", "M", "L", "XL"],
+      talla: ["S", "M", "L", "XL","XXL"],
       colores: ["Azul", "Negro", "Gris"],
       categoria: "Ropa",
       imagen: "ruta/imagen_pantalon.jpg",
@@ -25,7 +62,7 @@ const data = {
       nombre: "Camisa",
       descripcion: "Camisa de algodón de manga larga",
       precio: 30.0,
-      talla: ["S", "M", "L", "XL"],
+      talla: ["S", "M", "L", "XL","XXL"],
       colores: ["Blanco", "Azul", "Negro"],
       categoria: "Ropa",
       imagen: "ruta/imagen_camisa.jpg",
